9f1bbaf7621d76a597d458b965b3c4d53e6137bb
Israel Ramirez <israplenitud@gmail.com>
2019-10-17 18:20:49 -0500

    Modificando componente App

---
+++ src/components/App.vue
@@ -0,0 +1,53 @@
+<template>
+  <div class="App">
+    <Header :title="'Composition API'" />
+    <Search :search="state.search" @search="handleSearch" />
+    <p class="App-intro">Sharing a few of our favourite movies</p>
+    <div class="movies">
+      <Movie v-for="movie in state.movies" :movie="movie" :key="movie.imdbID" />
+    </div>
+  </div>
+</template>
+
+<script>
+  import { reactive, watch } from '@vue/composition-api';
+  import Header from './Header.vue';
+  import Search from './Search.vue';
+  import Movie from './Movie.vue';
+
+  const API_KEY = 'A5549D08';
+
+export default {
+  name: 'app',
+  components: {
+    Header, Search, Movie
+  },
+  setup() {
+    const state=reactive ({
+      search: 'Jocker',
+      loading: true,
+      movies: [],
+      errorMessage: null
+    });
+
+    watch(() => {
+      const MOVIE_API_URL = 'https://www.omdbapi.com/?s=${state.search}&apikey=${API_KEY}';
+
+      fetch(MOVIE_API_URL)
+        .then(response => response.json())
+        .then(jsonResponse => {
+          state.movies = jsonResponse.Search;
+          state.loading = false;
+        });
+    });
+
+    return {
+      state,
+      handleSearch(searchTerm) {
+        state.loading = true;
+        state.search = searchTerm;
+      }
+    };
+  }
+}
+</script>
