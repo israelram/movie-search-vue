5b2110910f3c24e04fea209e18f8aa39bb2009a9
Israel Ramirez <israplenitud@gmail.com>
2019-10-17 18:20:01 -0500

    Creando componente Movie

---
+++ src/components/Movie.vue
@@ -0,0 +1,23 @@
+<template>
+    <div class="movie">
+        <h2>{{ movie.Title }}</h2>
+        <div>
+            <img width="200" :src="movie.Poster" :alt="altText" />
+        </div>
+        <p>{{ movie.Year}}</p>
+    </div>
+</template>
+
+<script>
+import { computed } from '@vue/composition-api';
+
+export default {
+    name: "Movie",
+    props: ['movie'],
+    setup({ movie }) {
+        const altText = computed(() => 'The movie titled: ${movie.Title}');
+
+        return { altText };
+    }    
+};
+</script>
